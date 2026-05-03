1221576071089e9f1c70e62bf69a701912b38283
Graine25 <rgupta9@scu.edu>
2026-05-02 21:33:15 -0700

    eot3 swap init

--- src/xenia/gpu/vulkan/vulkan_trace_viewer_main.cc
+++ src/xenia/gpu/vulkan/vulkan_trace_viewer_main.cc
@@ -2,12 +2,13 @@
  ******************************************************************************
  * Xenia : Xbox 360 Emulator Research Project                                 *
  ******************************************************************************
- * Copyright 2022 Ben Vanik. All rights reserved.                             *
+ * Copyright 2020 Ben Vanik. All rights reserved.                             *
  * Released under the BSD license - see LICENSE in the root for more details. *
  ******************************************************************************
  */
 
 #include "xenia/base/logging.h"
+#include "xenia/base/main.h"
 #include "xenia/gpu/trace_viewer.h"
 #include "xenia/gpu/vulkan/vulkan_command_processor.h"
 #include "xenia/gpu/vulkan/vulkan_graphics_system.h"
@@ -16,13 +17,10 @@ namespace xe {
 namespace gpu {
 namespace vulkan {
 
-class VulkanTraceViewer final : public TraceViewer {
- public:
-  static std::unique_ptr<WindowedApp> Create(
-      xe::ui::WindowedAppContext& app_context) {
-    return std::unique_ptr<WindowedApp>(new VulkanTraceViewer(app_context));
-  }
+using namespace xe::gpu::xenos;
 
+class VulkanTraceViewer : public TraceViewer {
+ public:
   std::unique_ptr<gpu::GraphicsSystem> CreateGraphicsSystem() override {
     return std::unique_ptr<gpu::GraphicsSystem>(new VulkanGraphicsSystem());
   }
@@ -30,32 +28,49 @@ class VulkanTraceViewer final : public TraceViewer {
   uintptr_t GetColorRenderTarget(
       uint32_t pitch, xenos::MsaaSamples samples, uint32_t base,
       xenos::ColorRenderTargetFormat format) override {
-    // TODO(Triang3l): EDRAM viewer.
+    auto command_processor = static_cast<VulkanCommandProcessor*>(
+        graphics_system_->command_processor());
+    // return command_processor->GetColorRenderTarget(pitch, samples, base,
+    //                                               format);
     return 0;
   }
 
   uintptr_t GetDepthRenderTarget(
       uint32_t pitch, xenos::MsaaSamples samples, uint32_t base,
       xenos::DepthRenderTargetFormat format) override {
-    // TODO(Triang3l): EDRAM viewer.
+    auto command_processor = static_cast<VulkanCommandProcessor*>(
+        graphics_system_->command_processor());
+    // return command_processor->GetDepthRenderTarget(pitch, samples, base,
+    //                                               format);
     return 0;
   }
 
   uintptr_t GetTextureEntry(const TextureInfo& texture_info,
                             const SamplerInfo& sampler_info) override {
-    // TODO(Triang3l): Textures, but from a fetch constant rather than
-    // TextureInfo/SamplerInfo which are going away.
+    auto command_processor = static_cast<VulkanCommandProcessor*>(
+        graphics_system_->command_processor());
+
+    // auto entry_view =
+    //    command_processor->texture_cache()->Demand(texture_info,
+    //    sampler_info);
+    // if (!entry_view) {
+    //  return 0;
+    //}
+    // auto texture = entry_view->texture;
+    // return static_cast<uintptr_t>(texture->handle);
     return 0;
   }
-
- private:
-  explicit VulkanTraceViewer(xe::ui::WindowedAppContext& app_context)
-      : TraceViewer(app_context, "xenia-gpu-vulkan-trace-viewer") {}
 };
+
+int trace_viewer_main(const std::vector<std::string>& args) {
+  VulkanTraceViewer trace_viewer;
+  return trace_viewer.Main(args);
+}
 
 }  // namespace vulkan
 }  // namespace gpu
 }  // namespace xe
 
-XE_DEFINE_WINDOWED_APP(xenia_gpu_vulkan_trace_viewer,
-                       xe::gpu::vulkan::VulkanTraceViewer::Create);
+DEFINE_ENTRY_POINT("xenia-gpu-vulkan-trace-viewer",
+                   xe::gpu::vulkan::trace_viewer_main, "some.trace",
+                   "target_trace_file");
