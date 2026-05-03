78fe3ad823ab2423080be32f0c78e5c49385db84
Graine25 <rgupta9@scu.edu>
2026-05-03 12:14:26 -0700

    Update libav.lua

--- third_party/libav.lua
+++ third_party/libav.lua
@@ -1,3 +1,10 @@
 group("third_party")
   include("libav/libavcodec/premake5.lua")
   include("libav/libavutil/premake5.lua")
+
+  project("libavutil")
+    filter("platforms:Windows")
+      buildoptions({
+        "/wd4113", -- function parameter lists differ
+      })
+    filter({})
